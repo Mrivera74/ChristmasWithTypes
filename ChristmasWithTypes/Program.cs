7c3224fa2755da18661fc5c9118b92f0404082f4
Mark Rivera <mrivera7475@gmail.com>
2022-05-23 15:25:34 -0400

    fixed quotation error

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -8,13 +8,13 @@ namespace ChristmasWithTypes
         {
             var xmas = new Christmas();
 
-            var xmasDay = "Christmas.Day.Thursday";
+            var xmasDay = Christmas.Day.Thur;
 
             //TODO set Santa's name to Kris Kringle
             xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{"Boots", "Jeans", "Coat"}; 
+            xmas.Presents =  new string[3]{"Boots", "Jeans", "Coat"};
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = 10;
